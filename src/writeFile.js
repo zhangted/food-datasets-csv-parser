8fc1bb7bcddaba850c4f77c0fe6331b1b7ada4e3
Vadim <vadim.putrov@gmail.com>
2019-07-03 17:45:56 +0300

    move

---
+++ src/writeFile.js
@@ -0,0 +1,225 @@
+// const filePath = require('../files')
+const fs = require('fs')
+const PATH = require('path')
+const srcUtils = require('./utils')
+    //const { promisify } = require('util') // ?? it's utils of not *** Answer : NO. It's using for writing data in json
+    // const { promisify } = require('util')
+    // const _ = require('lodash')
+
+/**
+ * for makeReadable()
+ * @param {Object} data a json object
+ * */
+function makeReadable(data) {
+    var dataStr = JSON.stringify(data)
+    
+    // @TODO create an array with this rules and run them inside of the loop?
+    dataStr = dataStr.replace(/{"/g, '{ "')
+    dataStr = dataStr.replace(/{"/g, '{ " ')
+    dataStr = dataStr.replace(/},{/g, ' },\n{')
+    dataStr = dataStr.replace(/":/g, '": ')
+    dataStr = dataStr.replace(/,"/g, ',\n "')
+    return dataStr
+}
+
+/**
+ * Write in file
+ * @param {String} path
+ * @param {Object} data
+ */
+function writeFile(path, data) {
+    var dataStr = makeReadable(data)
+        //dataStr = '[' + dataStr + ']'
+        //console.log(dataStr)
+    fs.writeFile(path, dataStr, function(err) {
+        if (err)
+            return console.log(err)
+        console.info(path + ' file generated successfully!')
+    })
+}
+
+// @TODO maybe we can remove it and use play.js as a place for testing our methods?
+function test() {
+    // console.log(filePath["groceryFilePath"]);
+    // console.log(JSON.parse("src/data/Grocery/grocery.json"));
+    // console.log(grocery);
+    // writeFiles()
+    // console.log(typeof require(filePath.groceryFilePath));
+
+    console.log('ok')
+}
+
+// execute function
+// writeFiles()
+
+/**
+ * For fixPath()
+ * @param {String} path
+ */
+function fixPath(path) {
+    path = PATH.resolve(__dirname, path) // absolute path
+    if (path[-1] !== '/') { path = path + '/' } // path correction
+    return path
+}
+
+/**
+ * readData()
+ * @param {string} path
+ * @param {string} file
+ * */
+function readData(path, file) {
+    console.log(path + file);
+
+    let data = fs.readFileSync(path + file)
+    console.log(data);
+
+    let fileData = JSON.parse(data)
+    return fileData
+}
+
+/**
+ * @param {String} folderNamePath
+ * @param {String} file
+ * @param {Object} fileData
+ * @param {var} flag
+ * */
+function saveFile(folderNamePath, file, fileData, flag) {
+    var fileDataLength = fileData.length
+    for (var i = 0; i < fileDataLength; i++) {
+        var fileName = getFileName(file, fileData[i], flag, i)
+        var elementPath = folderNamePath + '/' + fileName
+        writeFile(elementPath, fileData[i])
+    }
+}
+
+/**
+ * @param {String} path
+ * @param {String} file
+ */
+function makeFolder(path, file) {
+    var folderName = file.slice(0, -5) + '_elements'
+    var folderNamePath = path + folderName
+    if (isDirectory(folderNamePath)) {
+        fs.mkdirSync(folderNamePath)
+    }
+    return folderNamePath
+}
+
+/**
+ * For splitObject
+ *
+ * @describe split large files into single elements
+ *
+ * @param {String} fullPath
+ * @param {var} flag
+ * @param {var} keys
+ * @param {var} callback
+ */
+ function splitObject(fullPath, flag = 1,  keys = [], callback) {
+     /*
+       flag=1 ==> name according to index
+       flag=0 ==> name according to "name" attribute
+     */
+     const file = PATH.basename(fullPath)
+     let path = PATH.parse(fullPath).dir
+
+     if (PATH.extname(file) !== '.json') {
+         console.log("Require .json file.")
+         return
+     }
+
+    path = fixPath(path)
+    let fileData = readData(path, file) // Reading data...
+    var folderNamePath = makeFolder(path, file) // new folder to save splitted files
+    saveFile(folderNamePath, file, fileData, flag) // saving files
+
+    if (callback instanceof Function) {
+        setTimeout(function() {
+            callback(folderNamePath, keys)
+        }, 1000)
+    }
+ }
+// execute function
+// splitObject()
+
+/**
+ * fixFileName()
+ * @param {string} fileName
+ */
+function fixFileName(fileName) {
+    fileName = fileName.replace(/ /g, '_') // Replace space with underscore
+    fileName = fileName.toLowerCase() // Maintain Uniformity
+    return fileName
+}
+
+//@TODO move to utils?
+/**
+ * isDirectory()
+ * @param {string} folderNamePath
+ *  */
+function isDirectory(folderNamePath) {
+    if (fs.existsSync(folderNamePath)) {
+        return false
+    }
+    return true
+}
+
+/**
+ * getFileName()
+ * @param {string} file
+ * @param {Object} fileData
+ * @param {var} flag
+ * @param {var} index
+ */
+function getFileName(file, fileData, flag, index) {
+    var fileName
+    if (flag === 1) fileName = index + '-' + file // for example: 23-someJsonFile.json
+    else fileName = fileData.name + '.json' // for example: someValueOfName.json
+    fileName = fixFileName(fileName)
+    return fileName
+}
+
+/**
+ * For combineObjects()
+ * @param {String} path Path of folder where all splitted files are stored
+ * @param {var} keys List of keys that are to be removed
+ */
+function combineObject(path, keys) {
+    path = fixPath(path)
+    var content = srcUtils.readAllFiles(path) //read all json files
+    content = updateContent(content, keys) //modifying structure
+    var fileNamePath = path + PATH.basename(path) + "_combined.json" // for example: elements_combined.json
+    writeFile(fileNamePath, content) //saving
+}
+
+// @TODO this looks pretty bad. I even don't know and don't want to know how it works. and it definately can be easily broken
+/**
+ * For updateContent()
+ * @param {var} content
+ * @param {var} keys
+ */
+function updateContent(content, keys) {
+
+    var len = content.length // @TODO I don't like this short variable name
+    
+    for (var itr = 0; itr < len; itr++) { // @TODO I don't like this short variable name
+        var elementLen = content[itr].length
+        for (var i = 0; i < elementLen; i++) {
+            for (var j = 0; j < keys.length; j++) {
+                //console.log(content[itr][i][keys[j]]);
+                delete content[itr][i][keys[j]]
+            }
+        }
+    }
+
+    return content
+}
+
+module.exports = {
+    writeFile,
+    test,
+    splitObject,
+    combineObject,
+    makeReadable,
+    readData
+}
