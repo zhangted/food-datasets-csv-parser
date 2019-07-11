feb5c4366c6e9d45552d36b0c2ede9c7c33948c3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-11 05:46:44 +0300

    tests

---
+++ examples/projects/tests/projects/UnitConvertion/files.teszt.js
@@ -0,0 +1,8 @@
+// /* global test, describe, it, expect, jest */
+// const { __generateId } = require('@utils')
+
+// describe('empty test', () => {
+//   test.skip('it is not snowing', () => {
+//     expect(false).toBe(0)
+//   })
+// })
