36e24be73e2ddca3ac21227c6ad5098d26e5d54a
Jan-Strum <janusbased@gmail.com>
2019-07-17 10:40:50 -0500

    Moved headers variable and returned it after createReadStream.

--- src/getHeaders.js
+++ src/getHeaders.js
@@ -4,15 +4,17 @@ const csv = require('csv-parser')
 // Pass in the path to the data from which you want to obtain the headers.
 const getHeaders = path => {
   const results = []
+  let headers
 
   createReadStream(path)
     .pipe(csv())
     .on('data', data => results.push(data))
     .on('end', () => {
-      const headers = Object.keys(results[0])
-
-      return headers
+      headers = Object.keys(results[0])
+      console.log(headers)
     })
+
+  return headers
 }
 
 module.exports = getHeaders
