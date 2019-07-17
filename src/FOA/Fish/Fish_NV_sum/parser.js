5556ec7edf01c87a271464b18878c5dc38830475
Jan-Strum <janusbased@gmail.com>
2019-07-17 10:03:24 -0500

    Submitting for bug fix.

--- src/FOA/Fish/Fish_NV_sum/parser.js
+++ src/FOA/Fish/Fish_NV_sum/parser.js
@@ -1,11 +1,12 @@
-const path = require('path')
+// const path = require('path')
 const getHeaders = require('../../../getHeaders')
-const { parseDirectoryFiles } = require('../../../fileSystem')
+// const { parseDirectoryFiles } = require('../../../fileSystem')
 
-const headers = getHeaders('./Fish_NV_sum (per 100 g EP)')
+const headers = getHeaders('./Fish_NV_sum (per 100 g EP).csv')
+console.log('parser.js:', headers)
 
-const directory = './'
+// const directory = './'
 
-const directoryPath = path.join(__dirname, directory)
+// const directoryPath = path.join(__dirname, directory)
 
-parseDirectoryFiles(directoryPath, headers)
+// parseDirectoryFiles(directoryPath, headers)
