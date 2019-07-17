202fa78f5ad8a2e2111a5cae1cb6dc417b70d19f
Andrew Serra <andy@serra.us>
2019-07-18 01:34:29 +0300

    import issue fixed

--- src/csvToJson.js
+++ src/csvToJson.js
@@ -1,10 +1,10 @@
 // @TODO soon we'll replace it carefully with similar code from generator that was perfected and clean up
 //https://github.com/GroceriStar/food-datasets-csv-parser/issues/23
-import { writeFile } from './writeFile'
-import { createReadStream } from 'fs'
-import csv from 'csv-parser'
-import { resolve } from 'path'
-import { joinPath } from './utils'
+import { writeFile } from './writeFile';
+import { createReadStream } from 'fs';
+import csv from 'csv-parser';
+import { resolve } from 'path';
+import utils from './utils';
 
 
 // @TODO I don't like how this file was previously created.
@@ -24,7 +24,7 @@ const fileWriter = (i, fileName, start, stop) => {
   // we can also create a method for path.join, so it wouldn't complicate our code
   // really bad line
   let jsonPath = `/projects/USFA/${folderName}/${fileName}${i}.json`
-  let combinedPath = joinPath([__dirname, jsonPath])
+  let combinedPath = utils.joinPath([__dirname, jsonPath])
   // --> if you reading it - then it's time for updating it :)
 
   writeFile(combinedPath, data)
