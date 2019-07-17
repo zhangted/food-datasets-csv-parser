79c899c5429fde3c0d0d4cf0f9283b05f02eea0c
Jan-Strum <janusbased@gmail.com>
2019-07-17 09:01:09 -0500

    Parser example for FISH_NV_sum.

---
+++ src/FOA/Fish/Fish_NV_sum/parser.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const getHeaders = require('../../../getHeaders')
+const { parseDirectoryFiles } = require('../../../fileSystem')
+
+const headers = getHeaders('./Fish_NV_sum (per 100 g EP)')
+
+const directory = './'
+
+const directoryPath = path.join(__dirname, directory)
+
+parseDirectoryFiles(directoryPath, headers)
