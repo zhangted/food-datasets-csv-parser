c43cb15bf1a04acd61fa90bab4c1f1e6d30c9c5e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-20 00:38:06 +0300

    Projects 3.0 USFA

---
+++ projects3.0/USFA/src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash'
+
+import {
+  Nutrient,
+  Derivation_Code,
+  Product,
+  Serving_Size
+} from '@files'
+
+import { __generateId, __generateDate } from '@utils'
+
+// @TODO connect with parsers???
+
+export {
+
+}
