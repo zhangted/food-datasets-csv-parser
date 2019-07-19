c456a09bfbb13c0cbc34474104f4ff3f7e364abd
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-20 00:32:35 +0300

    Projects 3.0 FC

---
+++ projects3.0/FoodComposition/src/constants/actionTypeConstants/models.js
@@ -0,0 +1,4 @@
+// export const FC_DEPARTMENTS = 'GS_DEPARTMENTS'
+// export const FC_GROCERY = 'GS_GROCERY'
+// export const FC_INGREDIENTS = 'GS_INGREDIENTS'
+// export const FC_USERS = 'GS_USERS'
---
+++ projects3.0/FoodComposition/src/index.js
@@ -0,0 +1,41 @@
+import _ from 'lodash'
+
+import {
+  FinlandDataSet,
+  FranceDataSet,
+  GermanyDataSet,
+  ItalyDataSet,
+  NetherlandsDataSet,
+  SwedenDataSet,
+  UnitedKingdomDataSet,
+  FullDataSet
+} from '@files'
+
+import { __generateId } from '@utils'
+
+
+// const { resolve } from 'path'
+//
+// const dataPath = './'
+//
+// const pathResolver = (path) => {
+//   return resolve(__dirname, dataPath + path)
+// }
+
+const displayFoodCompositionData = () => {
+  return ''
+}
+
+
+
+
+export {
+ FinlandDataSet,
+ FranceDataSet,
+ GermanyDataSet,
+ ItalyDataSet,
+ NetherlandsDataSet,
+ SwedenDataSet,
+ UnitedKingdomDataSet,
+ FullDataSet
+}
