eff4a5384331c6157e4ff15af60338f193e963de
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-08-25 03:06:31 +0300

    parser changes

--- projects3.0/USFA/src/parsers/Derivation_Code/parser.js
+++ projects3.0/USFA/src/parsers/Derivation_Code/parser.js
@@ -2,18 +2,23 @@ import { join } from 'path';
 
 import { parseDirectoryFiles } from '@groceristar/food-dataset-csv-parser';
 // import { parseDirectoryFiles, getHeaders } from '@groceristar/food-dataset-csv-parser'
-// const headers = getHeaders('./Fish_NV_sum (per 100 g EP).csv');
 
-const headers = ['derivation_code', 'Derivation_Descript'];
+const parseUSFADerivationCode = () => {
+  // const headers = getHeaders('./Fish_NV_sum (per 100 g EP).csv');
 
-// @TODO changes required.
-// we can use module resolver for this
-const directory = '@raw/Derivation_Code_Description';
-// const directory = '../../../raw/Derivation_Code_Description';
+  const headers = ['derivation_code', 'Derivation_Descript'];
 
-// joining path of directory
-const directoryPath = join(__dirname, directory);
+  // @TODO changes required.
+  // we can use module resolver for this
+  const directory = '@raw/Derivation_Code_Description';
+  // const directory = '../../../raw/Derivation_Code_Description';
 
-// @TODO I don't like that this scripts are
-// called as it is... looks un-cool
-parseDirectoryFiles(directoryPath, headers);
+  // joining path of directory
+  const directoryPath = join(__dirname, directory);
+
+  // @TODO I don't like that this scripts are
+  // called as it is... looks un-cool
+  parseDirectoryFiles(directoryPath, headers);
+};
+
+export default parseUSFADerivationCode;
