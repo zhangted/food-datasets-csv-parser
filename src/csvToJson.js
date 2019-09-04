a0b75bac9b8d56b9da73cf42f7a6f189f8dc809a
zhangted <tedcbook@gmail.com>
2019-09-04 00:46:28 -0400

    woopz

--- src/csvToJson.js
+++ src/csvToJson.js
@@ -58,6 +58,7 @@ const assign = (file, dataEntries) => {
     const jsonObjects = dataEntries.slice(start, stop);
     file[1] += i //add i to file name
     generate(file, jsonObjects);
+    file[1].slice(0,-1) //delete i from file name so nxt file can have proper i
   }
 };
 
