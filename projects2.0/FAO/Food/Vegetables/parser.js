3914bc6409771211db9c544547ac50af5388f1d9
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-10-02 22:10:25 +0300

    parser upd

--- projects2.0/FAO/Food/Vegetables/parser.js
+++ projects2.0/FAO/Food/Vegetables/parser.js
@@ -9,6 +9,18 @@ async function Main() {
   const rawFilePath = `${__dirname}/Vegetables.csv`;
   const headers = getHeaders(rawFilePath);
 
+  // I might have to include lines 5-14 insinde of an async function and await it.
+  // This will become clearer once parseDrectoryFiles is functional.
+
+  // @TODO this line is a good idea. For all of our csv parsers
+  // projects we have a separated folder with files + parser inside.
+  // we can make a root directory as default inside of `parserDirectoryFiles`
+  // and change it if passed another variable
+  const directory = './';
+
+  const directoryPath = path.join(__dirname, directory);
+
+  parseDirectoryFiles(directoryPath, headers);
 }
 
 Main();
